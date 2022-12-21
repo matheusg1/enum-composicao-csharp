6089949a906e39849ac8af899e68ee59da5c6b2c
matheus.silva <tecmatheusg@hotmail.com>
2022-12-21 11:51:52 -0300

    Básico sobre Enums

---
+++ HerancaComposicao/Program.cs
@@ -0,0 +1,28 @@
+﻿using HerancaComposicao.Entities;
+using HerancaComposicao.Enums;
+using System;
+
+namespace HerancaComposicao
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = Enums.OrderStatus.Delivered
+            };
+            Console.WriteLine(order);
+
+            //Conversão de enum para string
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            //Conversão de string para enum
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }
+    }
+}
