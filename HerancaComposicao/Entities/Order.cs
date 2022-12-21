a234eadfc55adf686bc888bcf4b7b40074769a80
matheus.silva <tecmatheusg@hotmail.com>
2022-12-21 15:00:16 -0300

    Exercicio baseado em diagrama UML

---
+++ HerancaComposicao/Entities/Order.cs
@@ -0,0 +1,24 @@
+﻿using HerancaComposicao.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerancaComposicao.Entities
+{
+    public class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        
+        public void addItem()
+        {
+
+        }
+        public void removeItem()
+        {
+            
+        }
+    }
+}
---
+++ HerancaComposicao/Enums/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerancaComposicao.Enums
+{
+    internal class OrderStatus
+    {
+    }
+}
